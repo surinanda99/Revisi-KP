73a8c6f4d199b9273813d8761507398482d8d89f
surinanda99 <91960911+surinanda99@users.noreply.github.com>
2024-08-24 19:27:35 +0700

    update API

--- app/Http/Controllers/API/PengumumanController.php
+++ app/Http/Controllers/API/PengumumanController.php
@@ -10,6 +10,38 @@ class PengumumanController extends Controller
 {
     public function index(){
         $pengumuman = Pengumuman::whereNotNull('published_at')->orderBy('published_at', 'desc')->get();
-        return response()->json($pengumuman);
+        $response = [];
+        foreach ($pengumuman as $item) {
+            $response[] = $this->formattedJson($item);
+        }
+        return response()->json($response);
+    }
+
+    public function show($id)
+    {
+        $pengumuman = Pengumuman::where('id', $id)
+            ->whereNotNull('published_at')
+            ->first();
+
+        if (!$pengumuman) {
+            return response()->json([
+                'message' => 'Pengumuman tidak ditemukan',
+            ], 404);
+        }
+
+        $response = $this->formattedJson($pengumuman);
+
+        return response()->json($response);
+    }
+
+    public function formattedJson($pengumuman)
+    {
+        return [
+            'id' => $pengumuman->id,
+            'judul' => $pengumuman->judul,
+            'penulis' => $pengumuman->user,
+            'konten' => $pengumuman->isi,
+            'published_at' => $pengumuman->published_at,
+        ];
     }
 }
